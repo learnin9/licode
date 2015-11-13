56c4387ce6feae4bf8f0a12fc6cb8b0585eca467
learnin9 <learnin9@gmail.com>
2015-11-13 23:17:40 +0800

    Update installCentOsDeps.sh

--- scripts/installCentOsDeps.sh
+++ scripts/installCentOsDeps.sh
@@ -44,7 +44,7 @@ check_proxy(){
 }
 
 install_apt_deps(){
-  sudo yum install wget git make gcc openssl-devel cmake pkgconfig nodejs boost-devel boost-regex boost-thread boost-system log4cxx-devel rabbitmq-server mongodb mongodb-server curl boost-test tar xz libffi-devel npm yasm java-1.7.0-openjdk gettext
+  sudo yum install wget patch git make gcc openssl-devel cmake pkgconfig nodejs boost-devel boost-regex boost-thread boost-system log4cxx-devel rabbitmq-server mongodb mongodb-server curl boost-test tar xz libffi-devel npm yasm java-1.7.0-openjdk gettext
   sudo chown -R `whoami` ~/.npm ~/tmp/
 }
 
